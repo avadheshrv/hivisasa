0eff92e214a707db793c7979b49f2e0a56778eff
avadheshrv <avadhesh_bhatt@rvtechnologies.c.in>
2016-10-18 17:38:06 +0530

    article detail

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,122 @@ var express = require('express');
 var router  = express.Router();
 var moment  = require('moment');
 var request = require('request');
-var app     = express();
-//var apiUrl  = app.get('apiUrl');
-var apiUrl  = 'http://api.hivisasa.com';
 
-//var mediaBaseUrl = app.get('mediaBaseUrl');
-var mediaBaseUrl = 'http://static-hivisasa-com.s3-accelerate.amazonaws.com';
+//api endpoint static urls
+var apiUrl          = 'http://api.hivisasa.com';
+var mediaBaseUrl    = 'http://static-hivisasa-com.s3-accelerate.amazonaws.com';
 
-/* GET home page. */
+/*
+ *  Route to render homepage
+ */
 router.get('/', function(req, res, next) {
-  res.render('index', { county: '' });
+    
+    var county = "national";
+    
+    var options = {
+        uri : apiUrl+'/articles/county/'+county,
+        method : 'GET'
+    }; 
+    var apiResponse = '';
+    request(options, function (error, response, body){
+        if (!error && response.statusCode == 200) {
+            apiResponse = body; 
+            apiResponse = JSON.parse(apiResponse);
+        }
+        
+        //code to get politics category posts to show in footer
+        var options_politics = {
+            uri : apiUrl+'/articles/'+county+'/politics',
+            method : 'GET'
+        }; 
+        var apiResponsePolitics = '';
+        request(options_politics, function (error, response, body){
+            if (!error && response.statusCode == 200) {
+                apiResponsePolitics = body; 
+                apiResponsePolitics = JSON.parse(apiResponsePolitics);
+            }
+            
+            //code to get business category posts to show in footer
+            var options_business = {
+                uri : apiUrl+'/articles/'+county+'/business',
+                method : 'GET'
+            }; 
+            var apiResponseBusiness = '';
+            request(options_business, function (error, response, body){
+                if (!error && response.statusCode == 200) {
+                    apiResponseBusiness = body; 
+                    apiResponseBusiness = JSON.parse(apiResponseBusiness);
+                }
+                
+                res.render('index',{
+                    'county':county,
+                    'moment': moment,
+                    'apiImgUrlPath':mediaBaseUrl,
+                    'articlesList':apiResponse,
+                    'articlesListPolitics':apiResponsePolitics,
+                    'articlesListBusiness':apiResponseBusiness,
+                    'mediaBaseUrl':mediaBaseUrl
+                });
+                
+            });
+        });
+    });
+});
+
+/*
+ *  Route to render specific county
+ */
+router.get('/:county', function(req, res){
+    var county = req.params.county;
+    
+    var options = {
+        uri : apiUrl+'/articles/county/'+county,
+        method : 'GET'
+    }; 
+    var apiResponse = '';
+    request(options, function (error, response, body){
+        if (!error && response.statusCode == 200) {
+            apiResponse = body; 
+            apiResponse = JSON.parse(apiResponse);
+        }
+
+        //code to get politics category posts to show in footer
+        var options_politics = {
+            uri : apiUrl+'/articles/'+county+'/politics',
+            method : 'GET'
+        }; 
+        var apiResponsePolitics = '';
+        request(options_politics, function (error, response, body){
+            if (!error && response.statusCode == 200) {
+                apiResponsePolitics = body; 
+                apiResponsePolitics = JSON.parse(apiResponsePolitics);
+            }
+            
+            //code to get business category posts to show in footer
+            var options_business = {
+                uri : apiUrl+'/articles/'+county+'/business',
+                method : 'GET'
+            }; 
+            var apiResponseBusiness = '';
+            request(options_business, function (error, response, body){
+                if (!error && response.statusCode == 200) {
+                    apiResponseBusiness = body; 
+                    apiResponseBusiness = JSON.parse(apiResponseBusiness);
+                }
+                
+                res.render('index',{
+                    'county':county,
+                    'moment': moment,
+                    'apiImgUrlPath':mediaBaseUrl,
+                    'articlesList':apiResponse,
+                    'articlesListPolitics':apiResponsePolitics,
+                    'articlesListBusiness':apiResponseBusiness,
+                    'mediaBaseUrl':mediaBaseUrl
+                });
+                
+            });
+        });
+    });
 });
 
 /*
@@ -108,8 +214,6 @@ router.get('/article/:county/:category/:articleId', function(req,res){
                 apiResponseRelated = JSON.parse(apiResponseRelated);
             }
             
-            
-            
             res.render('desktop/articleDetail',{
                 'county':county,
                 'moment': moment,
@@ -122,4 +226,46 @@ router.get('/article/:county/:category/:articleId', function(req,res){
     });
 });
 
+/*
+ *  Route to render article detail view from specific article id
+ */
+router.get('/article/:articleId', function(req,res){
+    var county      = '';
+    var category    = '';
+    var articleId   = req.params.articleId;
+    
+    var request = require('request');
+    var options = {
+        uri : apiUrl+'/articles/'+articleId,
+        method : 'GET'
+    }; 
+    var articleDetail = '';
+    request(options, function (error, response, body){
+        if(!error && response.statusCode == 200){
+            articleDetail = body; 
+            articleDetail = JSON.parse(articleDetail);
+        }
+        //code to get related posts
+        var options_related = {
+            uri : apiUrl+'/articles/related/'+articleId,
+            method : 'GET'
+        }; 
+        var apiResponseRelated = '';
+        request(options_related, function (error, response, body){
+            if (!error && response.statusCode == 200) {
+                apiResponseRelated = body; 
+                apiResponseRelated = JSON.parse(apiResponseRelated);
+            }
+            
+            res.render('desktop/articleDetailDirect',{
+                'county':apiResponseRelated.county,
+                'moment': moment,
+                'apiImgUrlPath':mediaBaseUrl,
+                'category':apiResponseRelated.category,
+                'articleDetail':articleDetail,
+                'relatedArticles':apiResponseRelated
+            });
+        });
+    });
+});
 module.exports = router;
